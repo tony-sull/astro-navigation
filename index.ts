c2f7008c729f1a5d40fd9d2309440a7686c1172d
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-18 17:49:34 -0500

    adding Breadcrumb support

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro'
+import Breadcrumbs from './src/Breadcrumbs.astro'
+import Navigation from './src/Navigation.astro'
+export type { PageFrontmatter } from './src/utils.js'
 
-export default MyComponent
+export default Navigation
+export { Breadcrumbs }
